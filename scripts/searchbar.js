ee88d8d0c6d7def42b381d3987241feca8ef2144
JasonDeng22 <tmdeng00@gmail.com>
2022-04-24 21:34:11 -0400

    search on champlist page

--- scripts/searchbar.js
+++ scripts/searchbar.js
@@ -1,12 +1,13 @@
 // https://www.geeksforgeeks.org/search-bar-using-html-css-and-javascript/
 // https://stackoverflow.com/questions/5338716/get-multiple-elements-by-id
+// https://www.geeksforgeeks.org/how-to-select-all-elements-that-contains-some-specific-css-property-using-jquery/
 function champListSearch() {
     let input = document.getElementById('searchbar');
     input.value = input.value.toLowerCase();
     let cards = document.getElementsByClassName('card');
     //console.log(cards);
     let names = document.querySelectorAll('[id=name]');
-    console.log(names);
+    //console.log(names);
     // loop through all the cards. if the text content of the <p> with id="name" includes the input from
     // search bar, then include that card in the DOM. Otherwise, don't display it
 
@@ -21,5 +22,12 @@ function champListSearch() {
 
     //input.value = "";
     // if every single card has display = none, then write to DOM the champ wasn't found
-
+    // let allNone = true;
+    // let row = document.getElementsByClassName('row');
+    // if (row[1].innerHTML.toLowerCase().includes("display: flex")) {
+    //     allNone = false;
+    // }
+    // if (allNone) {
+    //     $("#row").innerHTML = "<h3 style='color: white; text-align: center'>No Champs Found</h3>"
+    // }
 }
--- templates/championsList.php
+++ templates/championsList.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-
+<!-- https://codeburst.io/how-to-position-html-elements-side-by-side-with-css-e1fae72ddcc -->
 <html lang="en">
   <head>
     <meta charset="utf-8" />
